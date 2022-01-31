7a73e3d8b15c88a007eec5683e17d98dfbcb9161
AHauntedArmy <extreme15@hotmail.co.uk>
2022-01-31 15:50:04 +0000

    Add project files.

---
+++ BetaSlides/Plugin.cs
@@ -0,0 +1,79 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace BetaSlides
+{
+    /// <summary>
+    /// This is your mod's main class.
+    /// </summary>
+
+    /* This attribute tells Utilla to look for [ModdedGameJoin] and [ModdedGameLeave] */
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class BetaSlidesPlugin : BaseUnityPlugin
+    {
+        bool inRoom;
+
+        GameObject slideParent = null;
+
+        void Start()
+        {
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnEnable()
+        {
+            /* Set up your mod here */
+            /* Code here runs at the start and whenever your mod is enabled*/
+
+            if (inRoom && slideParent != null)
+                slideParent.SetActive(this.enabled);
+        }
+
+        void OnDisable()
+        {
+            /* Undo mod setup here */
+            /* This provides support for toggling mods with ComputerInterface, please implement it :) */
+            /* Code here runs whenever your mod is disabled (including if it disabled on startup)*/
+
+            if(slideParent != null)
+                slideParent.SetActive(false);
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            /* Code here runs after the game initializes (i.e. GorillaLocomotion.Player.Instance != null) */
+
+            slideParent = GameObject.Find("Level/Forest/TempSlides");
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is joined */
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            /* Activate your mod here */
+            /* This code will run regardless of if the mod is enabled*/
+
+            inRoom = true;
+
+            if (slideParent != null)
+                slideParent.SetActive(this.enabled);
+        }
+
+        /* This attribute tells Utilla to call this method when a modded room is left */
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            /* Deactivate your mod here */
+            /* This code will run regardless of if the mod is enabled*/
+
+            inRoom = false;
+
+            if (slideParent != null)
+                slideParent.SetActive(false);
+        }
+    }
+}
---
+++ BetaSlides/PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BetaSlides
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.ahauntedarmy.gorillatag.betaslides";
+        public const string Name = "BetaSlides";
+        public const string Version = "1.0.0";
+    }
+}
